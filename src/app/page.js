3c939a53927946f5ecf9605092145094d5b510ae
Aditya Sharma <sharma.aditya19@outlook.com>
2023-08-02 19:52:57 +0530

    added footer

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Body1 from "@/components/body1/Body1";
 import Highlights from "@/components/highlights/Highlights";
 import Yatras from "@/components/yatras/Yatras";
 import Footer from "@/components/footer/Footer";
+import Wander from "@/components/wander/Wander";
 import React from "react";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
     <Body1/>
     <Highlights/>
     <Yatras/>
+    <Wander/>
     <Footer/>
     </div>
   );
